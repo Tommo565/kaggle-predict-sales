c19dc0b663945a003789ab834ef447633348a4ad
Tom Ewing <ewingt1979@gmail.com>
2019-12-03 06:22:45 +0000

    Updated README and created config_example.py

---
+++ ml_app/config/__init__.py
@@ -0,0 +1,4 @@
+from config.config import (
+    gcp_token, bucket_name, import_data_folder, project_id,
+    test_bucket_name, test_import_data_folder
+)
---
+++ ml_app/config/config_example.py
@@ -0,0 +1,9 @@
+# Config
+gcp_token = './config/gcp_token.json'
+project_id = 'my_project'
+bucket_name = "my_bucket"
+import_data_folder = 'my_folder'
+
+# Unit Test Config
+test_bucket_name = 'my_test_bucket'
+test_import_data_folder = 'my_test_folder'
